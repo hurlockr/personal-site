d5bfce658c7b460aac99b6a4586c5298d99388ba
Ross Hurlock <jrlhurlock@gmail.com>
2023-10-03 15:18:11 -0400

    first commit. Made Hero component

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ross Hurlock Dev Portfolio",
+  description:
+    "This is a collection of projects that I made. I really hope you like them!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
